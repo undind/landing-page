0e0ebabf5f60790d7ec8fb1c3e19b285fa02ba59
SergeyAntonovDev <sergey-antonov-dev@mail.ru>
2020-05-30 15:14:14 +0300

    Fin sidebar logic

--- src/js/main.js
+++ src/js/main.js
@@ -1,35 +1,46 @@
 $(document).ready(function () {
     var hamburgerMenu = $('#navbar-responsive');
     var hamburgerButton = $('#navbar-button');
+    var navbarBackdrop = $('.navbar-backdrop');
+    var navbarClose = $('.navbar-toggler-close');
 
     function showMenu() {
         hamburgerMenu.addClass('fadeIn');
         hamburgerButton.addClass('show');
-        document.body.classList.add('open');
+        navbarBackdrop.removeClass('hidden');
+        navbarClose.removeClass('hidden')
+        $('body').addClass('open');
     }
 
     function hideMenu() {
         hamburgerMenu.removeClass('fadeIn');
         hamburgerButton.removeClass('show');
+        navbarBackdrop.addClass('hidden');
+        $('body').removeClass('open');
     }
 
     function timeOut() {
         hamburgerMenu.addClass('fadeOut');
-        hamburgerButton.removeClass('show');
 
         setTimeout(() => {
             hideMenu();
             hamburgerMenu.removeClass('fadeOut');
+            navbarClose.addClass('hidden')
         }, 400);
     }
 
     $(hamburgerButton).on('click', function (e) {
         e.preventDefault();
+        showMenu();
+    });
 
-        if (hamburgerButton.hasClass('show')) {
+    $(navbarClose).on('click', function () {
+        timeOut();
+    });
+
+    $(document).on('click', function (e) {
+        if (e.target.classList.contains('navbar-backdrop')) {
             timeOut();
-        } else {
-            showMenu();
         }
     });
 
