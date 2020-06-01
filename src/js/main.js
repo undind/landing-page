68af9edc22a41492da77cb2f2b7cca97a3e4b568
SergeyAntonovDev <sergey-antonov-dev@mail.ru>
2020-06-01 21:52:57 +0300

    Added animation on open dropdown

--- src/js/main.js
+++ src/js/main.js
@@ -8,7 +8,7 @@ $(document).ready(function () {
         hamburgerMenu.addClass('fadeIn');
         hamburgerButton.addClass('show');
         navbarBackdrop.removeClass('hidden');
-        navbarClose.removeClass('hidden')
+        navbarClose.removeClass('hidden');
         $('body').addClass('open');
     }
 
@@ -25,7 +25,7 @@ $(document).ready(function () {
         setTimeout(() => {
             hideMenu();
             hamburgerMenu.removeClass('fadeOut');
-            navbarClose.addClass('hidden')
+            navbarClose.addClass('hidden');
         }, 400);
     }
 
@@ -47,6 +47,31 @@ $(document).ready(function () {
     $(document).keyup(function (e) {
         if (e.key === 'Escape' && hamburgerButton.hasClass('show')) {
             timeOut();
+        }
+    });
+
+    //Dropdown navbar
+    var $dropdown = $('.dropdown-hovered');
+    var $dropdownToggle = $('#dropdow-main-toggle');
+    var $dropdownMenu = $('#dropdown-main-menu');
+    var showClass = 'show';
+
+    $(window).on('load resize', function () {
+        if (this.matchMedia('(min-width: 992px)').matches) {
+            $dropdown.hover(
+                function () {
+                    var $this = $(this);
+                    $this.addClass(showClass);
+                    $this.find($dropdownToggle).attr('aria-expanded', 'true');
+                    $this.find($dropdownMenu).addClass(showClass);
+                },
+                function () {
+                    var $this = $(this);
+                    $this.removeClass(showClass);
+                    $this.find($dropdownToggle).attr('aria-expanded', 'false');
+                    $this.find($dropdownMenu).removeClass(showClass);
+                }
+            );
         }
     });
 });
