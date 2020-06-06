f988189899025ce207805bba9665b2d8c435a13b
SergeyAntonovDev <sergey-antonov-dev@mail.ru>
2020-06-06 15:47:20 +0300

    Fin info slider

--- src/js/main.js
+++ src/js/main.js
@@ -77,3 +77,28 @@ $(document).ready(function () {
         }
     });
 });
+
+//Info slider
+$(document).ready(function () {
+    $('#recipeCarousel').carousel({
+        interval: 10000,
+    });
+    
+    $('.section__info .carousel .carousel-item').each(function () {
+        var minPerSlide = 3;
+        var next = $(this).next();
+        if (!next.length) {
+            next = $(this).siblings(':first');
+        }
+        next.children(':first-child').clone().appendTo($(this));
+
+        for (var i = 0; i < minPerSlide; i++) {
+            next = next.next();
+            if (!next.length) {
+                next = $(this).siblings(':first');
+            }
+
+            next.children(':first-child').clone().appendTo($(this));
+        }
+    });
+});
